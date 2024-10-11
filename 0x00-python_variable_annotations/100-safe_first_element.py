3d68ac572cc2f6a2d1bdf21075831b7b823ef3da
Walaakhkh <Walaakhalil98@gmail.com>
2024-10-11 21:49:06 +0000

    task 12

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Augment the following code with the correct duck-typed annotations
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples containing elements and their respective lengths.
+
+    Args:
+        lst (Iterable[Sequence]): A list (or iterable) of sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples where each tuple contains
+        an element from the input list and its length.
+    """
+    return [(i, len(i)) for i in lst]
