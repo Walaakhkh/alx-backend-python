4eabdbab56fb31812447728b21b14e7aa718ccf0
Walaakhkh <Walaakhalil98@gmail.com>
2024-10-11 21:18:43 +0000

    task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module defining variables with type annotations.
+"""
+
+# a is an integer with the value 1
+a: int = 1
+
+# pi is a float with the value 3.14
+pi: float = 3.14
+
+# i_understand_annotations is a boolean with the value True
+i_understand_annotations: bool = True
+
+# school is a string with the value "Holberton"
+school: str = "Holberton"
